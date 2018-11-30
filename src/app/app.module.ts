fad28c25c9e0e3cb4e814db157f376a0eb4a60c5
Oliver Steiner <oliver@mollo.ch>
2018-11-30 19:59:37 +0100

    Add Stylelint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SandboxComponent } from './sandbox/sandbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SandboxComponent
   ],
   imports: [
     BrowserModule,
