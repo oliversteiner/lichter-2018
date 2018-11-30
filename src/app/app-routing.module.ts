81c9be044848a2ab6b30d4f1beddc8c3793fc589
Oliver Steiner <oliver@mollo.ch>
2018-11-30 20:39:19 +0100

    Add Component mqtt-sandbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SandboxComponent} from './sandbox/sandbox.component';
+import {MqttSandboxComponent} from './mqtt-sandbox/mqtt-sandbox.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'sandbox', component: SandboxComponent
+    },
+    {
+        path: 'mqtt-sandbox', component: MqttSandboxComponent
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
+import { MqttSandboxComponent } from './mqtt-sandbox/mqtt-sandbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SandboxComponent
+    SandboxComponent,
+    MqttSandboxComponent
   ],
   imports: [
     BrowserModule,
