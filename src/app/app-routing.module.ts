ba9a2dc38cc6491facf09b5d618f8cfa94d95e74
Oliver Steiner <oliver@mollo.ch>
2018-12-01 17:44:21 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SandboxComponent} from './sandbox/sandbox.component';
 import {MqttSandboxComponent} from './mqtt-sandbox/mqtt-sandbox.component';
+import {LichtComponent} from './licht/licht.component';
+import {TimerComponent} from './timer/timer.component';
+import {SensorenComponent} from './sensoren/sensoren.component';
 
 const routes: Routes = [
-    {
-        path: 'sandbox', component: SandboxComponent
-    },
-    {
-        path: 'mqtt-sandbox', component: MqttSandboxComponent
-    },
+    {path: 'sandbox', component: SandboxComponent},
+    {path: 'mqtt-sandbox', component: MqttSandboxComponent},
+    {path: 'licht', component: LichtComponent},
+    {path: 'timer', component: TimerComponent},
+    {path: 'sensoren', component: SensorenComponent},
+
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lichter';
+  title = 'Lichter 2018';
 
 }
