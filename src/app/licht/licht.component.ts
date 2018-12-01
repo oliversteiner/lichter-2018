7e985265b0fae2624f675848484afc9882f4477f
Oliver Steiner <oliver@mollo.ch>
2018-12-01 17:44:46 +0100

    add routing

---
+++ src/app/licht/licht.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-licht',
+  templateUrl: './licht.component.html',
+  styleUrls: ['./licht.component.scss']
+})
+export class LichtComponent implements OnInit {
+
+  title = 'Lichtstatus';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/sensoren/sensoren.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sensoren',
+  templateUrl: './sensoren.component.html',
+  styleUrls: ['./sensoren.component.scss']
+})
+export class SensorenComponent implements OnInit {
+title = "Sensoren";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
