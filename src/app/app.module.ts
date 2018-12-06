faf23164665c78d3d8ba7db65e931cfc8dad0d6a
Oliver Steiner <oliver@mollo.ch>
2018-12-06 01:38:31 +0100

    Rename Navtab "Timers" to "Schaltuhr"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,27 +24,26 @@ export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
 
 
     // Dev
-    // clientId: 'lichter-2018',
-    // connectTimeout: 5000,
-    // hostname: '10.0.1.11',
-    // port: 8083,
-    // path: '/mqtt',
-    // username: 'schlossnet',
-    // password: 'schloss#28975-IUZ',
-
-    // Prod
     clientId: 'lichter-2018',
     connectTimeout: 5000,
-    hostname: 'iot-server.local',
+    hostname: '10.0.1.11',
     port: 8083,
     path: '/mqtt',
     username: 'schlossnet',
     password: 'schloss#28975-IUZ',
 
+    // Prod
+    // clientId: 'lichter-2018',
+    // connectTimeout: 500// hostname: 'iot-server.local',
+    // port: 8083,
+    // path: '/mqtt',
+    // username: 'schlossnet',
+    // password: 'schloss#28975-IUZ',
+
 
 };
 
-
+//
 @NgModule({
     declarations: [
         AppComponent,
