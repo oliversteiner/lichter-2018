6a0f7aac29af027fe2762324d3cad353248bbb49
Oliver Steiner <oliver@mollo.ch>
2018-12-06 02:09:36 +0100

    1.1.0

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -1,2 +1,2 @@
-// This file was generated on Do  6 Dez 2018 01:25:05 CET
-export const appVersion = '1.0.0';
+// This file was generated on Do  6 Dez 2018 02:09:35 CET
+export const appVersion = '1.1.0';
